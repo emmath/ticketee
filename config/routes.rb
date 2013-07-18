1a06189806ce800e4fd354c17c13455ceadb36bd
emmath <emmatrevena@gmail.com>
2013-07-18 09:20:07 -0700

    Add the ability to create admin users through the admin backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Ticketee::Application.routes.draw do
-  get "/admin/users/index"
+  namespace :admin do
+    root :to => "base#index"
+    resources :users
+  end
 
   devise_for :users
 
   root :to => "projects#index"
+
   resources :projects do
     resources :tickets
   end
